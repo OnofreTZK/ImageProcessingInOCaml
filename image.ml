eae6d85de15a038a1aab06b7a78221f6213e6ff3
Tiago Onofre <taraujonofre@gmail.com>
2021-07-31 00:57:59 -0300

    Started file manip

--- image.ml
+++ image.ml
@@ -1,18 +1,20 @@
+(** Defining Pixel*)
 type pixel = { 
   mutable red : int;
   mutable green : int;
   mutable blue : int;
 };;
 
+(** Defining Image *)
 type image = {
-  id : string;
+  mutable id : string;
   mutable row : int;
   mutable col : int;
-  max_value : int;
+  mutable max_value : int;
   mutable body: pixel array array
 };;
 
-
+(** Empty image for tests purposes *)
 let init = {
   id = "P3";
   row = 0;
@@ -20,6 +22,18 @@ let init = {
   max_value = 255;
   body = Array.make_matrix 0 0 {red = 0; green = 0; blue = 0}
 };;
+
+let read_image file_name img =
+  let read_channel = open_in file_name in
+  try
+    img.id <- input_line read_channel;
+    (** Transform  this in a list of strings *)
+    img.row, img.col <- int_of_string input_line read_channel;
+    img.max_value <- max_value;
+    close_in read_channel
+  with err -> close_in_noerr read_channel;
+    raise err;;
+
 
 
 
--- imageproc.ml
+++ imageproc.ml
@@ -1,7 +1,12 @@
 open Image
 
-let ppm_image = Image.init;;
-
+(** Initiating vars *)
 let init_message = "Init Image Processing..."
-
 let () = Printf.printf "%s\n" init_message 
+
+(** Passing file name *)
+let ppm_image_file_name = Array.get Sys.argv 1;;
+let () = Printf.printf "%s\n" ppm_image_file
+
+
+
