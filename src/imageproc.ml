29a22712065eac15ff13453d705cc16039d9d93c
Tiago Onofre <taraujonofre@gmail.com>
2021-08-04 21:42:25 -0300

    Argument verification

--- src/imageproc.ml
+++ src/imageproc.ml
@@ -1,19 +1,24 @@
 open Image
 
-(** Initiating vars *)
-let init_message = "Init Image Processing..."
-let () = Printf.printf "%s\n" init_message 
+(* Initiating vars *)
+let init_message = ">>> Initializing Image Processing...";;
+let () = Printf.printf "%s\n" init_message;;
 
-(** Passing file name *)
-let ppm_image_file_name = Array.get Sys.argv 1;;
-let () = Printf.printf "%s\n" ppm_image_file_name
+(* Veryfing argument *)
+let () = if Array.length Sys.argv < 2 then Printf.printf ">>> Please, give a ppm image as an argument!\n"; exit 0;;
+
+(* Passing file name *)
+let ppm_image = Sys.argv.(1);;
+
+let () = Printf.printf ">>> Input image: %s\n" ppm_image;;
+
+(*TODO Implement menu*)
 
 (** First Image test *)
 let test_image = Image.init;;
 
-let () = Image.read_image ~input_file: ppm_image_file_name ~img: test_image;;
-
-let () = Image.write_image ~img: test_image ~output_file: "test.ppm"
+let () = Image.read_image ~input_file: ppm_image ~img: test_image;;
+let () = Image.write_image ~img: test_image ~output_file: "test.ppm";;
 
 
 
