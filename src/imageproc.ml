aa49f8b9d09fe785c63bdb3188dee4678b90f0c5
Tiago <taraujonofre@gmail.com>
2021-08-13 01:38:29 -0300

    Removed ppx preprocessors and realized that a refactoration on main file is necessary -> still think on imperative way

--- src/imageproc.ml
+++ src/imageproc.ml
@@ -1,26 +1,18 @@
 open Image
 open Menu
 
-(* Initiating vars *)
-let init_message = ">>> Initializing Image Processing...";;
-let () = Printf.printf "%s\n" init_message;;
-
 (* Veryfing argument *)
 let () = if Array.length Sys.argv < 2 then Printf.printf ">>> Please, give a ppm image as an argument!\n"; exit 0;;
 
+let () = Printf.printf ">>> Initializing Image Processing\n%!"
+
 (* Passing file name *)
-let ppm_image = Sys.argv.(1);;
+let ppm_image = Sys.argv.(1)
+let () = Printf.printf ">>> Input image: %s\n" ppm_image
 
-let () = Printf.printf ">>> Input image: %s\n" ppm_image;;
-
-(** First Image test *)
-let test_image = Image.init;;
-
-let () = Image.read_image ~input_file: ppm_image ~img: test_image;;
-let () = Image.write_image ~img: test_image ~output_file: "test.ppm";;
-
-
-(*TODO Implement menu*)
-let () = Menu.init;; 
-
-
+(* First Image test *)
+let test_image = Image.init
+let () = Image.read_image ~input_file: ppm_image ~img: test_image
+(*let () = Image.write_image ~img: test_image ~output_file: "test.ppm"*)
+(* Menu *)
+let () = Menu.init;
