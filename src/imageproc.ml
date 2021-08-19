a4a44a884117843336c606a236a3f48405d50908
Tiago Onofre <taraujonofre@gmail.com>
2021-08-19 15:04:17 -0300

    Renamed functions

--- src/imageproc.ml
+++ src/imageproc.ml
@@ -14,17 +14,17 @@ let verify_args = (Array.length Sys.argv) < 2;;
 
 (* Menu *)
 let init validator =
-  let loop_cmd_control parse_func img = 
+  let loop_cmd_control parse_input parse_entry img = 
     let rec loop cmd = (* Menu loop *)
       if cmd = Exit then Printf.printf "Closing program...\n" (* Finish program *)
-      else Commands.parse_entry img cmd |> fun () -> loop (Scanf.scanf "%s\n" parse_func) in (* Rest of inputs *)
-    loop (Scanf.scanf "%s\n" parse_func)
+      else parse_entry img cmd |> fun () -> loop (Scanf.scanf "%s\n" parse_input) in (* Rest of inputs *)
+    loop (Scanf.scanf "%s\n" parse_input)
   in
   match validator with
     | true -> Printf.printf ">>> Please, give a ppm image as an argument!\n%!"
     | false -> Printf.printf "%s\n%!" str_init |> fun () -> Printf.printf "%s\n%!" Parameters.str_menu |>
               fun () -> init_image Sys.argv.(1) |> 
-              loop_cmd_control Commands.cmd_to_entry
+              loop_cmd_control Commands.cmd_to_entry Commands.parse_entry
 ;;
 
 let () = init verify_args;;
